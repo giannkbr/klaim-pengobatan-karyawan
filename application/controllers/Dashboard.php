f052f64ee283490c2bcc79f0b7cc94e4b287a533
Gian Akbar <akbarigi20@gmail.com>
2022-07-17 21:36:27 +0700

    v.1.0 [add total employee on dashboard]

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -6,7 +6,7 @@ class Dashboard extends CI_Controller
     {
         parent::__construct();
         is_logged_in();
-        $this->load->model('klaim_m');
+        $this->load->model(['employee_m', 'klaim_m']);
     }
 
     public function index()
@@ -14,6 +14,7 @@ class Dashboard extends CI_Controller
         $data['title'] = 'Dashboard';
         $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
         $data['company'] = $this->db->get('company')->row_array();
+        $data['totalEmployee'] = $this->employee_m->getEmployee()->num_rows();
         $this->template->load('backend', 'backend/dashboard', $data);
     }
 }
